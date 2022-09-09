1937bccd91f5a817f329241c038d90d35d3240fd
Andrewharber <harber.andrew@ymail.com>
2022-09-09 17:58:12 -0400

    commit tree d530a80f6c4e73e96e2ed73af01c5ebcec6948ef

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let palindrome = word.toLowerCase()
+  let reverse = palindrome.split('').reverse().join('');   
+  return (palindrome == reverse)  
 }
-
+  
 /* 
   Add your pseudocode here
-*/
+assign variable to word.toLowerCase()
+assign variable to apply chain of split() reverse() join() to palindrome variable
+compare 
+return */
 
 /*
   Add written explanation of your solution here
+  assignment to palindrome variable to hold value of word
+  assignment to reverse variable to hold value of word reversed
+  then return is using equality operator to verify word is a palindrome returning a boolean value
 */
 
 // You can run `node index.js` to view these console logs
